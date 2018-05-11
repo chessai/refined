7dfcb51486fd39923e6a3d6a88facc63997a0285
chessai <chessai1996@gmail.com>
2018-05-11 12:17:02 -0400

    re-structured

--- Refined/Foo.hs
+++ Refined/Foo.hs
@@ -9,6 +9,16 @@ import Refined
 import Control.Monad (guard)
 import Data.List (replicate)
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+bar :: Refined NonEmpty (Map Int Int)
+bar = $$(refineTH (Map.singleton 0 1))
+
+-- this will fail to compile
+--bar' :: Refined NonEmpty (Map Int Int)
+--bar' = $$(refineTH (Map.empty))
+
 foo :: [Bool]
 foo = foldl1 (\x acc -> x >>= guard >> acc)
   ($$(refineTH (replicate 3 True)) :: Refined NonEmpty [Bool])
---
+++ Refined/Internal/Orphan.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE DeriveLift         #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell    #-}
+
+{-| This module contains orphan 'Lift' instances for more available compile-time checking of predicates.
+-}
+
+module Refined.Internal.Orphan () where
+
+import Data.Map.Internal (Map(..))
+import Data.Set.Internal (Set(..))
+import qualified Language.Haskell.TH.Syntax as TH
+
+deriving instance (TH.Lift k, TH.Lift v) => TH.Lift (Map k v)
+deriving instance (TH.Lift v) => TH.Lift (Set v)
+
